7be62cfd2b74f29f5f7c9f1dea703c6930322321
MaikelPilligua06 <mpilligua@institutmvm.cat>
2026-03-06 12:37:01 +0100

    Se añade numero a la tabla y se corrige el ejecutable para insertar el usuario

--- proyecto/scripts/usuario.php
+++ proyecto/scripts/usuario.php
@@ -4,12 +4,9 @@ require_once "models/db.php";
 $db = conectar();
 $passwordAdmin = password_hash("mvp", PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO Usuarios (nombre, apellido, tipoDocumento, numeroDocumento, correoElectronico, contrasenia, edad, genero, peso, altura, objetivo, fechaDeAlta, id_entrenador) 
-VALUES ('usuario', 'instiut', 'DNI', '48042187A', 'mvp@institutmvm.cat', '$passwordAdmin', 20, 'Masculino', 70, 1.40, 'estabilidad', '2026-02-27', 1)";
-$db->execute($sql);
+$sql = "INSERT INTO Usuarios (nombre, apellido, numeroTelefono, tipoDocumento, numeroDocumento, correoElectronico, contrasenia, edad, genero, peso, altura, objetivo, fechaDeAlta, id_entrenador) 
+VALUES ('usuario', 'instiut', '+34 6310042233', 'DNI', '48042187A', 'mvp@institutmvm.cat', '$passwordAdmin', 20, 'Masculino', 70, 1.40, 'estabilidad', '2026-02-27', 1)";
+$db->exec($sql);
 echo "Usuario insertado correctamente.";
 exit;
 ?>
-
-
-
