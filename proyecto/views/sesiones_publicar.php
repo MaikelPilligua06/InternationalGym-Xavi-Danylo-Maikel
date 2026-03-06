3fa802863c5fd652804ea99caace402f84402faf
MaikelPilligua06 <mpilligua@institutmvm.cat>
2026-03-06 15:48:44 +0100

    Correción de tags sin cerrar y error de nombres

--- proyecto/views/sesiones_publicar.php
+++ proyecto/views/sesiones_publicar.php
@@ -3,17 +3,22 @@
 ?>
 <h1>Crear nueva publicación</h1>
 
-<form action="index.php?controller=Sesiones&action=publicar"
-      method="post" enctype="multipart/form-data">
+<form action="index.php?controller=Sesiones&action=publicar" method="POST" >
 
     <label>Nombre de la Sesión</label>
         <input type="text" name="nombre"/></br>
-
+    <label>Tipo de la Sesión</label>
+    <select name="tipoDeClases">
+        <option value="Cardio">Cardio</option>
+        <option value="Cycling">Cycling</option>
+        <option value="trenSuperior">Tren Superior</option>
+        <option value="trenInferior">Tren inferior</option>
+    </select>
     <label>Fecha de la Sesión</label>
         <input type="date" name="fechaClases"/>
 </br>
     <label>Duración de la clase</label>
-        <input type="tine" name="duración"
+        <input type="time" name="duracion"/>
     <p>
     </br>
         <label>Descripción de tu sesión:</label><br>
@@ -29,7 +34,4 @@
 <?php
     include 'footer.php';
     ?>
-
-
-
 
