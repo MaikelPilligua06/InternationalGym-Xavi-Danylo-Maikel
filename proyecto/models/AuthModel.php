b942f401c5dfbfabe27d7cb2de7a96fbaf881609
MaikelPilligua06 <mpilligua@institutmvm.cat>
2026-03-06 15:53:10 +0100

    Acceso a la página para el entrenador
    Added login functionality for 'entrenador' users.

--- proyecto/models/AuthModel.php
+++ proyecto/models/AuthModel.php
@@ -4,6 +4,7 @@ require_once "db.php";
 class AuthModel {
 
     public function login($correo, $password) {
+        // Login para usuario
         $db = conectar();
         $stmt = $db->prepare("
 SELECT id, correoElectronico, contrasenia
@@ -17,6 +18,21 @@ WHERE correoElectronico = :correo
         if ($user && password_verify($password, $user['contrasenia'])) {
             return $user;
         }
+
+        // Login para entrenador
+        $stmt = $db->prepare("
+SELECT id, correoElectronico, contrasenia
+FROM Entrenadores
+WHERE correoElectronico = :correo
+");
+        $stmt->execute([":correo"=>$correo]);
+
+        $entrenador = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($entrenador && password_verify($password, $entrenador["contrasenia"])){
+            return $entrenador;
+        }
+
 
         return false;
     }
