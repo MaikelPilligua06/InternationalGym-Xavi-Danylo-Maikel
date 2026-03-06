792f39187b744ae11d8450704ef454254accd4ca
MaikelPilligua06 <mpilligua@institutmvm.cat>
2026-03-06 17:23:33 +0000

    A la hora de entrar a la página cargan las 2 listas a la vez, todas y la del usuario

--- proyecto/controllers/EjerciciosController.php
+++ proyecto/controllers/EjerciciosController.php
@@ -3,22 +3,25 @@
 require_once "models/EjerciciosModel.php";
 
 class EjerciciosController{
-    public function listadoEjercicios(){
+    public function listadoEjercicios() {
         $model = new EjerciciosModel();
+        $usuarioId = $_SESSION['id'];
         $ejercicio = $model->getAll();
+        $lista = ($usuarioId) ? $model->informacionUsuario($usuarioId) : [];
         require "views/entrnamientoUsuario.php";
     }
+
     public function addEjercicio(){
         $model = new EjerciciosModel();
-        $ejercicio = new Ejercicios($datos);
-        $model->guardar($ejercicio);
+        $usuario = $_SESSION['id'];
+        $model->guardar($_GET['id'], $usuario);
         header("Location: index.php?controller=Ejercicios&action=listadoEjercicios");
         exit();
 
     }
     public function infoEjercicio(){
         $model = new EjerciciosModel();
-        $ejercicios = $model->getById($_SESSION["id"]);
+        $ejercicios = $model->getById($_GET["id"]);
         require "views/ejercicios_ver.php";
     }
 }
