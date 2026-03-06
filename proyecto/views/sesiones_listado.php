09636396af7dc634bb827ab7caeb4f0fe5b0c9a4
MaikelPilligua06 <mpilligua@institutmvm.cat>
2026-03-06 14:26:46 +0100

    Añadimos el formulario para crear sesiones
    Added a section for publications with links to create and view user publications.

--- proyecto/views/sesiones_listado.php
+++ proyecto/views/sesiones_listado.php
@@ -5,6 +5,15 @@
 </head>
 <body>
 <?php include 'header.php'; ?>
+<div class="Publicaciones">
+    <h2>Lista de Publicaciones</h2>
+    <a href="index.php?controller=Sesiones&action=ver">
+        <img src="" alt="Crear Publicacion"/>
+    </a>
+    <a href="index.php?controller=Sesiones&action=misPublicaciones">
+        <p>Ver mis publicaciones</p>
+    </a>
+</div>
 
 <h1>Sesiones</h1>
 
