ae8e333452f22171b63d2a94e54372e146ea8a56
MaikelPilligua06 <mpilligua@institutmvm.cat>
2026-03-06 15:54:22 +0100

    Variables para definir mejor una sesion

--- proyecto/models/SesionesDeClases.php
+++ proyecto/models/SesionesDeClases.php
@@ -2,18 +2,22 @@
 class SesionesDeClases
 {
     public $id;
+    public $nombre;
     public $tipoDeClases;
     public $fechaClases;
     public $duracion;
     public $id_entrenador;
+    public $descripcion;
 
     public function __construct($datos = [])
     {
 
         $this->id = $datos["id"] ?? null;
+        $this->nombre = $datos["nombre"] ?? null;
         $this->tipoDeClases = $datos["tipoDeClases"] ?? null;
         $this->fechaClases = $datos["fechaClases"] ?? null;
         $this->duracion = $datos["duracion"] ?? null;
         $this->id_entrenador = $datos["id_entrenador"] ?? null;
+        $this->descripcion = $datos["descripcion"] ?? null;
     }
 }
