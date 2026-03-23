39e803b10ab9bed8b8b9e5b9fcddebcc6f20c53e
Maikel <mpilligua@institutmvm.cat>
2026-03-23 19:36:04 +0100

    Funciones de agregar, eliminar y ver plato

--- proyecto/models/AlimentacionModel.php
+++ proyecto/models/AlimentacionModel.php
@@ -13,13 +13,37 @@ class AlimentacionModel{
         }
         return $alimentacion;
     }
-    public function alimentacionUsuario ($usuario){
-        $db = conectar();
-        $stmt = $db->query("
+    //
+    //public function alimentacionUsuario ($usuario){
+    //    $db = conectar();
+     //   $stmt = $db->query("
             // Falta hacer un select donde se recogerian los siguientes datos:
             // select donde se recogerian el id del usuario y de los platos entre 3 tablas. 
-        ");
-        $stmt->execute(['id' => $usuario]);
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+       // ");
+        //$stmt->execute(['id' => $usuario]);
+        //return $stmt->fetch(PDO::FETCH_ASSOC);
+    //}
+    public function getPlato($id){
+        $db = conectar();
+        $stmt = $db->prepare("SELECT * FROM Alimentacion WHERE id = :id");
+        $stmt->execute(['id' => $id]);
+        $fila = $stmt->fetch(PDO::FETCH_ASSOC);
+        return new Alimentacion($fila);
+    }
+    public function agregarPlato($id, $usuarioId){
+        $db = conectar();
+        $stmt = $db->prepare("INSERT INTO Usuario_Alimentacion (id_usuario, id_alimentacion) VALUES (:id, :id_alimentacion)");
+        $stmt->execute([
+            ':id' => $usuarioId,
+            ':id_alimentacion' => $id
+        ]);
+    }
+    public function eliminarPlatoUsuario($id, $usuarioId){
+        $db = conectar();
+        $stmt = $db->prepare("DELETE FROM Usuario_Alimentacion WHERE id_alimentacion = :id AND id_usuario = :id_usuario");
+        $stmt->execute([
+            ':id' => $usuarioId,
+            ':id_alimentacion' => $id
+        ]);
     }
 }
