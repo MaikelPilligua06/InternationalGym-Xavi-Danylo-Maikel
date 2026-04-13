b1d5beae0ecfd5a887987f8ee4e3248cf8c60746
isard <mpilligua@institutmvm.cat>
2026-04-13 20:07:54 +0200

    Estilo sesiones

--- proyecto/views/Entrenadores/entrenadorDatos.php
+++ proyecto/views/Entrenadores/entrenadorDatos.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Entrenador - Sesiones</title>
+    <link rel="stylesheet" href="views/styles.css">
+</head>
 <?php
     include 'views/header.php';
     ?>
@@ -8,11 +14,26 @@
         <p>Dispobilidad horaria: <?php echo $entrenador->disponibilidadHoraria;?></p>
     <p><?php echo $entrenador->descripcion;?></p>
 </div>
-<div>
+
+<div class="carrusel-movil">
     <h2>Sesiones del entrenador</h2>
-    <?php foreach ($sesiones as $sesion): ?>
-        <p><?php $sesion->nombre?></p>
-    <?php endforeach; ?>
+    <div class="pista">
+
+        <?php foreach ($sesiones as $sesion): ?>
+            <div class="tarjeta-movil">
+                <a href="index.php?controller=Sesiones&action=getId&id=<?= $sesion->id ?>">
+                    <h4>Nombre de la clase: <?= $sesion->nombre ?></h4>
+                    <h4>TIpo de clases: <?= $sesion->tipoDeClases ?></h4>
+                    <p><strong>Fecha:</strong> <?= $sesion->fechaClases ?></p>
+                    <p><strong>Duracion:</strong> <?= $sesion->duracion ?></p>
+                    <p><strong>Entrenador:</strong> <?= $sesion->id_entrenador ?></p>
+                </a>
+                <a href="index.php?controller=Sesiones&action=apuntarme&id=<?= $sesion->id ?>">
+                    <button>Apuntate</button>
+                </a>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </div>
 <form>
     <button>Cambiar de entrenador</button>
