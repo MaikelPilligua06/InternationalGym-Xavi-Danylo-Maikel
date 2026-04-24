8a9294454d58cafac0751dfc88735e37503e14c4
Danylo <dpeliak@institutmvm.cat>
2026-04-24 17:55:59 +0200

    Entn

--- proyecto/views/Entrenadores/todosLosEntrenadores.php
+++ proyecto/views/Entrenadores/todosLosEntrenadores.php
@@ -2,17 +2,19 @@
 <html>
 <head>
     <link rel="stylesheet" href="views/entrenador.css">
+    <link rel="stylesheet" href="views/entrenadores.css"
 </head>
-<body>
+<div>
 <?php
     include 'views/header.php';
 ?>
 
 <div class="contenido">
+
+    <h1>Tu Entrenador es</h1>
         <?php foreach($lista as $entrenadorUsuario): ?>
             <a href="index.php?controller=Entrenador&action=getEntrenador&id=<?= $entrenadorUsuario->id;?>">
                 <div class="texto">
-                    <h1>Tu Entrenador es</h1>
                     <h2><?php echo $entrenadorUsuario->nombreEntrenador, '', $entrenadorUsuario->apellido;?></h2>
                     <h2><?php echo $entrenadorUsuario->correoElectronico;?></h2>
                     <h2><?php echo $entrenadorUsuario->descripcion;?></h2>
@@ -24,13 +26,19 @@
         </a>
         <?php endforeach; ?>
 </div>
-<div>
-        <h2>Todos los entrenadores</h2>
+    <hr/>
+<div class="contenido-oscuro">
         <?php foreach ($entrenadores as $entrenador): ?>
         <a href="index.php?controller=Entrenador&action=getEntrenador&id=<?= $entrenador->id;?>">
+
+            <div class="texto"
+            <h3>Todos los entrenadores</h3>
             <h3><?php echo $entrenador->nombreEntrenador, ' ', $entrenador->apellido;?></h3>
-            <p><?php echo $entrenador->descripcion;?></p>
-            <img src="">
+            <h3><?php echo $entrenador->descripcion;?></h3>
+        </div>
+            <div class="imagen">
+            <img src="views/gymFotos/entrenador.png" alt="Entrenador"">
+            </div>
         </a>
         <?php endforeach; ?>
 </div>
