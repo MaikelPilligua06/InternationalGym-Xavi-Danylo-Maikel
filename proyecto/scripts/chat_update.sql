545a7c65dc590094a2ec103961cedb474022227a
Xavi <xbusca@institutmvm.cat>
2026-05-06 12:26:55 +0200

    Error solucionado de login

--- proyecto/scripts/chat_update.sql
+++ proyecto/scripts/chat_update.sql
@@ -5,3 +5,7 @@ AFTER id_entrenador;
 UPDATE Chat
 SET enviado_por = 'usuario'
 WHERE enviado_por IS NULL OR enviado_por = '';
+
+ALTER TABLE Alimentacion
+ADD COLUMN id_usuario INT NULL AFTER id,
+ADD COLUMN fecha DATE NULL AFTER id_usuario;
